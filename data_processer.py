8a8e5bc25c1d7f4a33db35907de7a5830f2b5019
thealexlittle <alexlittle2college@gmail.com>
2022-07-27 21:13:10 -0700

    Added DN supprot to transcrpits and a req file

--- data_processer.py
+++ data_processer.py
@@ -7,11 +7,66 @@ from datetime import date
 
 # Populate Occurence of morpheme
 
-def findTimeStamps_TH():
+def captureSentence(i, captions):
+    punc = ".!?"
+
+    # Read line until punctuation found
+    s = ""
+    for j in range(i,len(captions)):
+        rt = captions[j].text
+        if rt.isnumeric():
+            continue
+        t = re.sub(r'\d+$', '', rt)
+        s = s + " " +t
+        for p in punc:
+            if p in s:
+                print(i,j)
+                return (j,s)
+    #Return sentence 
+
+
+def findTimeStamps_DN(fp):
+    negation_dict = ['No ', 'Not ', 'None', 'No one', 'Nobody', 'Nothing', 'Neither', 'Nowhere', 'Never', 'Hardly', 'Scarcely', 'Barely', 'Doesn`t', 'Isn`t', 'Wasn`t', 'Shouldn`t', 'Wouldn`t', 'Couldn`t', 'Won`t', 'Can`t', 'Don`t']
+
     lines = []
-    for caption in webvtt.read("cleantext/WillSmith.vtt"):
+    captions = webvtt.read(fp)
+    for i in range(len(captions)):
+        #text = caption.text.splitlines()[0]
+        ts = captions[i].start
+        d = captureSentence(i, captions)
+        i = d[0]+1
+        text = d[1]
+        
+        # Skip music notes
+        if "♪" in text:
+            continue
+        
+        # Check if any negation words are in the line
+        c = 0 
+        for n in negation_dict:
+        # Add Line if it occurs, (maybe code for occurences of multiple)
+            if n.lower() in text.lower():
+                c = c + 1
+            
+            if c >= 2:
+                break
+        
+        # Store result in tuple, (ts,t,oc)
+        if c == 1:
+            lines.append((ts, text, 0))
+        elif c >= 2:
+            lines.append((ts, text, 1))
+        else: 
+            continue
+
+    return lines    
+
+
+def findTimeStamps_TH(fp):
+    lines = []
+    for caption in webvtt.read(fp):
         text = caption.text.splitlines()[0]
-        print(text)
+        #print(text)
         # Skip music notes
         if "♪" in text:
             continue
@@ -20,9 +75,8 @@ def findTimeStamps_TH():
         p = '^[Tt][Hh]| [Tt][Hh]'
         c = len(re.findall(p,text))
         for i in range(c):
-            t = (caption.start, text)
+            t = (caption.start, text, 0)
             lines.append(t)
-    print(lines)
     return lines
 
 def csvData(lines, d, n):
@@ -35,9 +89,10 @@ def csvData(lines, d, n):
         for line in lines:
             ts = line[0]
             cap = line[1]
-            print(cap)
+            occ = line[2]
+            #print(cap)
             writer.writerow({
-                fieldnames[0]: 0, 
+                fieldnames[0]: occ, 
                 fieldnames[1]: 'black', # race of interviewee
                 fieldnames[2]: 'marriage', # subject
                 fieldnames[3]: 53, # age of interviewee
@@ -47,8 +102,10 @@ def csvData(lines, d, n):
                 fieldnames[7]: cap
             })
 
+fp = ["cleantext\WillSmith.vtt", "cleantext\Maya_Angelou_Interview_(2013-5-19).en.vtt"]
 d = date(2021,10,13)
-ts = findTimeStamps_TH()
-csvData(ts,d,"TH")
 
+ts = findTimeStamps_DN(fp[1])
+csvData(ts,d,"DN")
 
+""
