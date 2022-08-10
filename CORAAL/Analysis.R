b515789ed176bea3af18ff77ad02fc16a2fd4ef8
thealexlittle <alexlittle2college@gmail.com>
2022-08-10 15:22:15 -0700

    Removed n word analysis, added Linear models to accompany all graphs

--- CORAAL/Analysis.R
+++ CORAAL/Analysis.R
@@ -28,14 +28,12 @@ p.contractions <- "(?:am |[sedoi])n(?:'t|ot)"
 # p.past.reg <- "([Ww]eren't|[Ww]asn't|[Cc]an't)"
 p.reductions <- "musta|woulda|shoulda|mighta|gonna|hafta|tryna|sposta|finna|gotta|wanna|oughta|cause|til"
 p.aint <- "ain't"
-p.n_word <- "nigga"
 
 #Search CORAAL for utterances
 # be.utts <- coraal.search(p.be)
 contraction.utts <- coraal.search(p.contractions)
 red.utts <- coraal.search(p.reductions)
 aint.utts <- coraal.search(p.aint)
-n_word.utts <- coraal.search(p.n_word)
 
 # Read in be utts from handcoded csv
 be.utts <- read.csv('b.csv')
@@ -53,7 +51,6 @@ md <- md %>% mutate(be = query2col(be.utts),
                     contra = query2col(contraction.utts),
                     red = query2col(red.utts),
                     aint = query2col(aint.utts),
-                    n_word = query2col(n_word.utts),
                     lines.Count = query2col(coraal)
                     )
 
@@ -64,12 +61,10 @@ md <- md %>% mutate(be.rat.lines = be / lines.Count,
                     contra.rat.lines = contra / lines.Count,
                     red.rat.lines = red / lines.Count,
                     aint.rat.lines = aint / lines.Count,
-                    n_word.rat.lines = n_word / lines.Count,
                     be.rat.words = be / CORAAL.Word.Count,
                     contra.rat.words = contra / CORAAL.Word.Count,
                     red.rat.words = red / CORAAL.Word.Count,
                     aint.rat.words = aint / CORAAL.Word.Count,
-                    n_word.rat.words = n_word / CORAAL.Word.Count
 )
 
 
@@ -114,15 +109,19 @@ ggplot(md.combo, aes(x = Age, y = be.rat.lines, color=InterviewTime)) +
   labs(title="Usage of Invariant 'be' given a subject's age",
        x = "Age When Interviewed",
        y = "Ratio of be to lines")
-
+lm.age.be <- lm(Age ~ be.rat.lines, data = md.combo )
+summary(lm1=.age.be)
 
 # Plotting Birth Age and Usage of Be
 ggplot(md.combo, aes(x = Year.of.Birth, y = be.rat.lines, color=InterviewTime)) +
   geom_point() +
   geom_smooth(method = 'lm') +
-  labs(title = "Usage of Be compared to Year of birth in rececnt interviews",
+  labs(title = "Usage of Be compared to Year of Birth",
         x = "Year of Birth",
         y = "Useage of Be")
+
+lm.DOB.be <- lm(Age ~ be.rat.lines, data = md.combo )
+summary(lm.DOB.be)
 
 # Plot Year of birth against reduction usage
 ggplot(md.combo, aes(x = Year.of.Birth, y = red.rat.lines, color=InterviewTime)) +
@@ -132,6 +131,16 @@ ggplot(md.combo, aes(x = Year.of.Birth, y = red.rat.lines, color=InterviewTime))
        x = "Birth Year of Subject", 
        y = "Ratio of reduction by lines")
 
+ggplot(md.combo, aes(x = Age, y = red.rat.lines, color=InterviewTime)) +
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  labs(title="Usage of reduction given a subject's age when interviewed", 
+       x = "Age of Subject", 
+       y = "Ratio of reduction by lines")
+
+lm.DOB.red <- lm(Age ~ be.rat.lines, data = md.combo )
+summary(lm.DOB.red)
+
 # Plot use of Year of birth against aint usage
 ggplot(md.combo, aes(x = Year.of.Birth, y = aint.rat.lines, color=InterviewTime)) +
   geom_point() +
@@ -140,14 +149,15 @@ ggplot(md.combo, aes(x = Year.of.Birth, y = aint.rat.lines, color=InterviewTime)
        x = "Birth Year of Subject", 
        y = "Ratio of 'aint' usage by lines")
 
-#Plot of of year of birth against n_word usage
-ggplot(md.combo, aes(x = Year.of.Birth, y = n_word.rat.lines, color=InterviewTime)) +
+ggplot(md.combo, aes(x = Age, y = aint.rat.lines, color=InterviewTime)) +
   geom_point() +
   geom_smooth(method = 'lm') +
-  labs(title="Usage of n given a subject's age", 
-       x = "Birth Year of Subject", 
-       y = "Ratio of n usage by lines")
+  labs(title="Usage of aint given a subject's age", 
+       x = "Age of Subject when interviewed", 
+       y = "Ratio of 'aint' usage by lines")
 
+lm.DOB.aint<- lm(Age ~ be.rat.lines, data = md.combo )
+summary(lm.DOB.aint)
 
 
 
